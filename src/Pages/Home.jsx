b1bbd602434c4d85863257d0332a298a7785ff79
AnderH4ck <anderson2020tasilla@gmail.com>
2024-07-02 20:15:12 -0500

    Hero

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Hero from "../components/Hero/Hero";
 
 function Home() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Hero />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
