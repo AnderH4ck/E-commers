468ee45ab07e7d76b6fd9e3ace74737208c1a90b
AnderH4ck <anderson2020tasilla@gmail.com>
2024-07-03 20:45:32 -0500

    Card Terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home';
-
 function App() {
   return (
     <BrowserRouter>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
-
+import Products from "../components/Products/Products";
+import AOS from "aos";
+import "aos/dist/aos.css"
 function Home() {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }),[];
   return (
     <div>
       <Navbar />
       <Hero />
+      <Products />
     </div>
   );
 }
